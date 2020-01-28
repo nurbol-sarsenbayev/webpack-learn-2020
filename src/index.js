f2c5b5fb4d5a78e7c54fae66335c400368c067e5
Nurbol Sarsenbayev <n.sarsenbayev@infin-it.kz>
2020-01-28 14:06:33 +0600

    add image by file-loader

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import Post from './Post'
 import './styles/main.css'
 import json from './assets/json';
+import WebpackLogo from './assets/webpack-logo.png'
 
 const post = new Post('The webpack post')
 console.log(post.toString())
 console.log('JSON', json)
+console.log('WebpackLogo', WebpackLogo);
--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif)$/,
+                use: ['file-loader'],
             }
         ]
     }
