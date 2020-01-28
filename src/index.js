04fd5db2bc5c4b554c5d6e291d73a0abfc2620ae
Nurbol Sarsenbayev <n.sarsenbayev@infin-it.kz>
2020-01-28 15:01:08 +0600

    add xml by xml-loader

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Post from './Post'
 import './styles/main.css'
 import json from './assets/json';
+import xml from './assets/data.xml';
 import WebpackLogo from './assets/webpack-logo.png'
 
 const post = new Post('The webpack post')
 console.log(post.toString())
 console.log('JSON', json)
+console.log('XML', xml)
 console.log('WebpackLogo', WebpackLogo);
--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,10 @@ module.exports = {
                 test: /\.(ttf|woff|woff2|eot)$/,
                 use: ['file-loader'],
             },
+            {
+                test: /\.xml$/,
+                use: ['xml-loader'],
+            },
         ]
     }
 }
