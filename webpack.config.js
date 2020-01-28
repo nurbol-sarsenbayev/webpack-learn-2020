d680c86678e39ef17aae837ae546df2a76c74f17
Nurbol Sarsenbayev <n.sarsenbayev@infin-it.kz>
2020-01-28 18:56:25 +0600

    copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -25,6 +26,12 @@ module.exports = {
             template: './index.html',
         }),
         new CleanWebpackPlugin(),
+        new CopyWebpackPlugin([
+            {
+                from: path.resolve(__dirname, 'src', 'favicon.ico'),
+                to: path.resolve(__dirname, 'dist'),
+            },
+        ]),
     ],
     optimization: {
         splitChunks: {
