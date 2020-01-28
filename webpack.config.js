81a08f7b01ae120086f6348c5475be26796d7cca
Nurbol Sarsenbayev <n.sarsenbayev@infin-it.kz>
2020-01-28 13:10:03 +0600

    added webpack context

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,11 @@ const HTMLWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
+    context: path.resolve(__dirname, 'src'),
     mode: 'development',
     entry: {
-        main: './src/index.js',
-        analytics: './src/analytics.js',
+        main: './index.js',
+        analytics: './analytics.js',
     },
     output: {
         filename: '[name].[contenthash].js',
@@ -14,7 +15,7 @@ module.exports = {
     },
     plugins: [
         new HTMLWebpackPlugin({
-            template: './src/index.html',
+            template: './index.html',
         }),
         new CleanWebpackPlugin(),
     ],
