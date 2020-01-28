e7922e84e435dfc1594c3905c8e8af4b312fce66
Nurbol Sarsenbayev <n.sarsenbayev@infin-it.kz>
2020-01-28 13:37:12 +0600

    add json

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Post from './Post'
 import './styles/main.css'
+import json from './assets/json';
 
 const post = new Post('The webpack post')
 console.log(post.toString())
+console.log('JSON', json)
