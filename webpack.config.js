83ab82a99b45637726ff0e00c0b537724ac0f5de
Nurbol Sarsenbayev <n.sarsenbayev@infin-it.kz>
2020-01-28 19:20:33 +0600

    set process.env.NODE_ENV

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const HTMLWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: 'development',
